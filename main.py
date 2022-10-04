b9a54c086fa1942536a6f2160861b812eb33df62
badrik0077 <107414974+badrik0077@users.noreply.github.com>
2022-10-04 12:41:04 +0500

    some comment

--- main.py
+++ main.py
@@ -5,112 +5,134 @@ import json
 app = Flask(__name__)
 
 
-def get_value_from_db(sql):
+def get_data_by_sql(sql):
     with sqlite3.connect("netflix.db") as connection:
         connection.row_factory = sqlite3.Row
         result = connection.execute(sql).fetchall()
-        return result
 
-
-def search_by_title(title):
-    sql = f""" SELECT * FROM netflix 
-    WHERE title = '{title}' 
-    ORDER BY release_year desc LIMIT 1 """
-
-    result = get_value_from_db(sql)
-    for item in result:
-        return dict(item)
+    return result
 
 
 @app.get("/movie/<title>/")
-def search_by_title_view(title):
-    result = search_by_title(title=title)
-    return jsonify(result)
+def step_1(title):
+    result = {}
+    for item in get_data_by_sql(sql=f'''
+            select title, country, release_year, listed_in as genre, description 
+            from netflix 
+            where title = '{title}'  
+            order by release_year desc limit 1 
+            '''):
+        result = dict(item)
 
-@app.get("/movie/<year1>/to/<year2>/")
-def search_date_view(year1, year2):
-    sql = f"""
-    select title, release_year
-    from netlix
-    where release_year between '{year1}' and '{year2}'
-    limit 100
-    """
+    return app.response_class(
+        json.dumps(result, ensure_ascii=False, indent=4),
+        mimetype="application/json",
+        status=200
+    )
+
+
+@app.get("/movie/<int:year1>/to/<int:year2>/")
+def step_2(year1, year2):
+    result = {}
+    sql = f'''
+        select * from netflix
+        where release_year between {year1} and {year2} 
+        limit 100 
+        '''
     result = []
-    for item in get_value_from_db(sql=sql):
+
+    for item in get_data_by_sql(sql):
         result.append(dict(item))
 
-    return jsonify(result)
+    return app.response_class(
+        json.dumps(result, ensure_ascii=False, indent=8),
+        mimetype="application/json",
+        status=200
+    )
 
 
 @app.get("/rating/<rating>/")
-def search_rating_view(rating):
-    my_dict = {
+def step_3(rating):
+    my_rating = {
         "children": ("G", "G"),
         "family": ("G", "PG", "PG-13"),
         "adult": ("R", "NC-17")
     }
     sql = f"""
-    select title, rating, description
-    from netlix
-    where rating in {my_dict.get(rating, ("R", "R"))}
-    """
+        select * from netflix
+        where rating in {my_rating.get(rating, ("R", "R"))}
+        """
+
     result = []
-    for item in get_value_from_db(sql=sql):
+
+    for item in get_data_by_sql(sql):
         result.append(dict(item))
 
-    return jsonify(result)
+    return app.response_class(
+        json.dumps(result, ensure_ascii=False, indent=8),
+        mimetype="application/json",
+        status=200
+    )
 
 
-@app.get("/rating/<rating>genre/<genre>/")
-def search_genre_view(genre):
+@app.get("/genre/<genre>/")
+def step_4(genre):
     sql = f"""
-    select *
-    from netlix
-    where listed_in like "%{genre}"
-    order by release_year desc
-    limit 10
-    """
+        select show_id, type from netflix
+        where listed_in like '%{str(genre).title()}%' 
+        group by
+        """
+
     result = []
-    for item in get_value_from_db(sql=sql):
+
+    for item in get_data_by_sql(sql):
         result.append(dict(item))
 
-    return jsonify(result)
+    return app.response_class(
+        json.dumps(result, ensure_ascii=False, indent=8),
+        mimetype="application/json",
+        status=200
+    )
 
-@app.get("/movie/<year1>/to/<year2>/")
-def search_double_name(name1, name2):
-    sql = f"""
-    select "cast"
-    from netlix
-    where "cast" LIKE "%{name1}% and "cast" like "%{name2}%
-    """
-    result = []
+
+def step_5(name1='Rose McIver', name2='Ben Lamb'):
+    sql = f'''
+        select "cast" from netflix 
+        where "cast" like '%{name1}%' and "cast" like '%{name2}' 
+        '''
+
     names_dict = {}
-    for item in get_value_from_db(sql=sql):
-        names = set(dict(item).get("cast").split(",")) - set([name1, name2])
+
+    for item in get_data_by_sql(sql):
+        result = dict(item)
+
+        names = set(result.get('cast').split(", ")) - set([name1, name2])
 
         for name in names:
-            names_dict[str(name).strip()] = names+dict.get(name, 0) + 1
+            names_dict[name.strip()] = names_dict.get(name.strip(), 0) + 1
+
+    print(names_dict)
 
     for key, value in names_dict.items():
         if value >= 2:
-            result.append(key)
+            print(key)
 
-    return jsonify(result)
 
-def step_6(typ, year, genre):
+def step_6(types='Movie', year=2020, genre='Horror'):
     sql = f"""
-    select title, description, listed_in
-    from netlix
-    where type = "{typ}"
-    and release_year = "{year}",
-    and listed_in like "%{genre}%"
-    """
+            select title, description, listed_in
+            from netflix
+            where type = "{types}"
+            and release_year = "{year}",
+            and listed_in like "%{genre}%"
+            """
     result = []
 
-    for item in get_value_from_db(sql):
+    for item in get_data_by_sql(sql):
         result.append(dict(item))
 
-    return json.dumps(result, ensure_ascii=False, indent=4),
+    return result
 
-if __name__ == "__main__":
-    app.run()
+
+if __name__ == '__main__':
+    print(step_5())
